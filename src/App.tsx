540b62dca47da2ca51153739bd1d23cd2b1e027b
Haruya Okatani <okatani4869@gmail.com>
2025-05-10 15:40:32 +0900

    crypto.randomUUID非対応のブラウザがあるため、v4()で対応

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import { useEffect } from "react";
 import AppRoutes from "./routes/index.tsx"
 import { ContextsProvider } from "./contexts/contexts"
 import { BrowserRouter } from 'react-router-dom'
+import { v4 as uuidV4 } from 'uuid';
 
 function App() {
     useEffect(() => {
         // 初回アクセス時にuserIdをlocalStorageに保存
         if (!localStorage.getItem("userId")) {
-            localStorage.setItem("userId", crypto.randomUUID());
+            localStorage.setItem("userId", uuidV4());
         }
     }, []);
     return (
