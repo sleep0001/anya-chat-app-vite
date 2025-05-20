fabf297f206fab865e5cba30b570a0fc7e6a5104
Haruya Okatani <okatani4869@gmail.com>
2025-05-20 22:57:14 +0900

    import遅延解除

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 // src/routes/index.tsx
 import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom'
-// import PageLayout from '../components/layout/PageLayout.tsx'
+import PageLayout from '../components/layout/PageLayout.tsx'
 import { Layout } from 'antd';
 // import ChatRoom from '../components/layout/ChatRoom.tsx';
-// import Lobby from '../components/layout/Lobby.tsx';
+import Lobby from '../components/layout/Lobby.tsx';
 
-const PageLayout = lazy(() => import('../components/layout/PageLayout.tsx'));
-const Lobby = lazy(() => import('../components/layout/Lobby.tsx'));
+// const PageLayout = lazy(() => import('../components/layout/PageLayout.tsx'));
+// const Lobby = lazy(() => import('../components/layout/Lobby.tsx'));
 const ChatRoom = lazy(() => import('../components/layout/ChatRoom.tsx'));
 
 export default function AppRoutes() {
