e4b8a075b7b4e0b2007725a6fa33d0eef3abac53
Haruya Okatani <okatani4869@gmail.com>
2025-05-23 00:12:46 +0900

    スマホでの描画に多少対応

--- src/components/common/CardComponent.tsx
+++ src/components/common/CardComponent.tsx
@@ -9,9 +9,9 @@ const CardComponent = ({ card }: Props) => {
     return (
         <div>
             <img
-                src={"https://www.onepiece-cardgame.com/images/cardlist/card/" + card.expansion.name + "-" + formatThreeDigitString(card.number) + ".png"}
+                src={`https://www.onepiece-cardgame.com/images/cardlist/card/${card.expansion.name}-${formatThreeDigitString(card.number)}.png`}
                 alt={card.name}
-                style={{ width:"150px", height:"auto" }}
+                style={{ width: "100%", height: "auto", maxWidth: "150px" }}
             />
         </div>
     )
--- src/components/layout/CardList.tsx
+++ src/components/layout/CardList.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Card } from '../../types/CardType';
 import { getCards } from '../../hooks/cardAPI';
-import { List } from 'antd';
 import CardComponent from '../common/CardComponent';
 
 
@@ -14,8 +13,9 @@ const CardList = () => {
         <div
             style={{
                 display: "grid",
-                gridTemplateColumns: "repeat(auto-fill, minmax(150px, 1fr))",
-                gap: "1rem"
+                gridTemplateColumns: "repeat(auto-fit, minmax(clamp(90px, 20vw, 150px), 1fr))",
+                gap: "0.5rem",
+                justifyItems: "center",
             }}
         >
             {cards.map(card => (
